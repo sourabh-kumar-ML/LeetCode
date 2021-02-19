c81e1537dd582193fbce984f293c5e02284aa4c8
sourabh-kumar-ML <55852771+sourabh-kumar-ML@users.noreply.github.com>
2021-02-20 00:40:29 +0530

    Add files via upload

---
+++ String/isPalindrome.py
@@ -0,0 +1,8 @@
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        if x<0:
+            return False
+        else:
+            if str(x) == str(x)[::-1]:
+                return True
+            else : return False
